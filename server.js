78ead5431bf62ce322fe625255c322eadcb6a8a7
Madyan Eka <madyan.eka95@gmail.com>
2018-09-29 06:41:20 +0700

    Rendering Templates with Data

--- server.js
+++ server.js
@@ -1,22 +1,24 @@
 const express = require('express');
+const hbs = require('hbs');
 
 let app = express();
 
+app.set({'view engine': 'hbs'})
 app.use(express.static(__dirname + '/public')); // __dirname === root dir
 
 app.get('/', (req, res) => {
-  // res.send('<h1>Hello Express</h1>');
-  res.send({
-    name: 'Madyan',
-    likes: [
-      'Biking',
-      'Villages'
-    ]
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome to my website.',
+    currentYear: new Date().getFullYear()
   });
 });
 
 app.get('/about', (req, res) => {
-  res.send('About Page');
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+    currentYear: new Date().getFullYear()
+  });
 });
 
 // /bad - send back json with errorMessage
