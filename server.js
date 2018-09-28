3fd24b07b4c4f0d6f2b1161c8e09390e1a511333
Madyan Eka <madyan.eka95@gmail.com>
2018-09-29 06:21:47 +0700

    Creating Web Server Creating a web server and add some semicolons at the end of each code block.

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 let app = express();
 
+app.use(express.static(__dirname + '/public')); // __dirname === root dir
+
 app.get('/', (req, res) => {
   // res.send('<h1>Hello Express</h1>');
   res.send({
@@ -10,18 +12,20 @@ app.get('/', (req, res) => {
       'Biking',
       'Villages'
     ]
-  })
+  });
 });
 
 app.get('/about', (req, res) => {
-  res.send('About Page')
-})
+  res.send('About Page');
+});
 
 // /bad - send back json with errorMessage
 app.get('/bad', (req, res) => {
   res.send({
     errorMessage: 'Unable to handle request.'
-  })
-})
+  });
+});
 
-app.listen(3000);
+app.listen(3000, () => {
+  console.log('Server is up on port 3000.')
+});
